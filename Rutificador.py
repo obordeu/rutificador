74fe050ee1c4c5f550d28672977a0eb1a984e3df
Olivia Bordeu Gazmuri <oli.bordeu@gmail.com>
2017-03-23 17:04:55 -0400

    Add files via upload

---
+++ Rutificador.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+'''
+╔═╗┬  ┬┬  ┬┬┌─┐  ╔╗ ┌─┐┬─┐┌┬┐┌─┐┬ ┬
+║ ║│  │└┐┌┘│├─┤  ╠╩╗│ │├┬┘ ││├┤ │ │
+╚═╝┴─┘┴ └┘ ┴┴ ┴  ╚═╝└─┘┴└──┴┘└─┘└─┘
+'''
+# DATE: 14-Mar-2017
+# ACTION: Gets rut from chile.rutificador.com 
+
+import distance
+import requests
+
+import time
+from time import sleep
+
+
+
+
+def getRUT(iterator):
+	iterator['rutificador']=None
+	iterator['nombreRutificador']=None
+
+	nombre=iterator['nombre']
+
+
+	url = 'https://chile.rutificador.com/'
+	client = requests.session()
+	client.get(url)
+	csrftoken = client.cookies['csrftoken']
+	url = url + 'get_generic_ajax/'
+
+	data = {
+		'entrada': entrada,
+		'csrfmiddlewaretoken': csrftoken,
+		}
+	headers = {
+		'Referer': url,
+		}
+	try:
+		response = client.post(url, data=data, headers=headers)
+	except:
+		print("Connection refused by the server..")
+		print("A dormir por 5 segundos")
+		print("ZZzzzz...")
+		time.sleep(5)
+		print("Nice, ahora sigamos...")
+		response = client.post(url, data=data, headers=headers)
+		results=[]
+		if response.json()['status']=='success':
+			for x in response.json()['value']:
+				dist=distance.levenshtein(nombre,x['name'])
+				results.append((x['name'],dist,x['rut']))
+			distances=sorted(results, key=lambda palabra: palabra[1])
+			if len(distances)!=0:
+				iterator['rutificador']=distances[0][2]
+				iterator['nombreRutificador'] = distances[0][0]
+	return iterator
+
+
+def main():
+	# This is the main function of the program
+	data={
+	'nombre': 'GARY MEDEL SOTO',
+	'apellido1': 'MEDEL',
+	'apellido2': 'SOTO',
+	'nombres': 'GARY'
+	}
+
+	output=getRUT(data)
+	print output
+
+
+if __name__ == '__main__':
+    main()
+
+
